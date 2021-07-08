655fc65de30db9b30a97d3ac86e300986d51b49d
Ahmed Mesih <74631008+AhmedMesih@users.noreply.github.com>
2021-07-08 22:13:13 +0300

    modifications for mish bbox percent

--- models/common.py
+++ models/common.py
@@ -4,8 +4,13 @@ import math
 import torch
 import torch.nn as nn
 
-from mish_cuda import MishCuda as Mish
-
+try:
+    from mish_cuda import MishCuda as Mish
+    
+except:
+    class Mish(nn.Module):  # https://github.com/digantamisra98/Mish
+        def forward(self, x):
+            return MishFunction.apply(x)
 
 def autopad(k, p=None):  # kernel, padding
     # Pad to 'same'
